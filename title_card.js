da7ea316dc88937afe2563567bb2b02cc51594cb
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-04 16:05:30 -0700

    Auto stash before merge of "master" and "origin/master"

--- title_card.js
+++ title_card.js
@@ -1,6 +1,6 @@
 // Christian make the title card go away after the user clicks start game with a fade away effect.
 
 // Listens to see when the jumbotron button is pushed
-document.getSelection(p.jumbotron-button).addEventListener("click", function(){
-
+document.getElementsByClassName("jumbotron-button").addEventListener("click", function(){
+ alert("hey bud");
 })
