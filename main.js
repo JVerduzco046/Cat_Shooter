500f1d4c353e426e4cd667f00d2d84779c415011
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-04 18:13:04 -0700

    accepted incoming changes
    main js files now have comments instead of changes

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-<<<<<<< Updated upstream
 // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy)
 
-=======
 // // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
 // document.getElementsByClassName(enemy-area).style.transform = "translate(100px)";
->>>>>>> Stashed changes
 
 
 
