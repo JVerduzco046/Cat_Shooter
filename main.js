a73374a82092d20a5d1de309147fc850a73ad7c5
Maria Rizo <mrizo036@west-mec.org>
2019-09-04 17:19:08 -0700

    Auto stash before merge of "master" and "origin/master"

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-<<<<<<< Updated upstream
 // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy)
-
-=======
-// // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
-// document.getElementsByClassName(enemy-area).style.transform = "translate(100px)";
->>>>>>> Stashed changes
+document.getElementsByClassName("jumbotron-button").onclick = function(){
+    if 
+}
 
 
 
 
 
-// anthony makes the enemies maria makes move side to side and shoot at random intervals
+// anthony makes the enemies (maria makes move side to side) shoot at random intervals
