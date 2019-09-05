627e68edfc361270e952cb9e840771d64fff5526
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-05 16:01:55 -0700

    Doc Clean-up
    Cleans up the document from the built in git branching.

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
 
-=======
 // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy)
-document.getElementsByClassName("jumbotron-button").onclick = function(){
-    if 
-}
+// document.getElementsByClassName("jumbotron-button").onclick = function(){
+//     if{};
+// }
 
 // // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
 // document.getElementsByClassName(enemy-area).style.transform = "translate(100px)";
->>>>>>> master
 
 
 
