47c41d04399813ca134ac048812fd09628e50b4f
Bryan Vargas <BVargas949@gmail.com>
2019-09-04 18:19:02 -0700

    Javascript was made to move the player left and right
    The png is not popping up.

--- animation.js
+++ animation.js
@@ -1,1 +1,33 @@
 // Bryan animate the assetes moving by arrow keys and shoot by the space bar
+
+const gameWidth = 1450;
+const gameHeight = 750;
+
+const gameState = {
+    playerX: 0,
+    playerY: 0,
+};
+
+function setPosition($el,x ,y){
+    $el.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+function createPlayer($container){
+gameState.playerX = gameWidth/2;
+gameState.playerY = gameHeight-50;
+const $player =document.createElement("img");
+$player.src = "cat.png";
+$player.className = "player";
+$container.appendChild($player);
+setPosition($player, gameState.playerX, gameState.playerY);
+}
+
+function init(){
+    const $container = document.querySelector("game");
+    createPlayer($container);
+}
+
+
+init();
+
+window.addEventListener("keydown", onkeydown);
