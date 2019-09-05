277765ac1892426ebc95f1b8f98ad906a8ca01dd
Anthony Gradillas <AGradillas025@west-mec.org>
2019-09-04 18:14:37 -0700

    (ATTEMPT) at making ships shoot

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-<<<<<<< Updated upstream
-// Maria make the enemies appear in 4 rows(1 row is the same kind of enemy)
-
-=======
-// // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
-// document.getElementsByClassName(enemy-area).style.transform = "translate(100px)";
->>>>>>> Stashed changes
+// Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
 
 
 
 
 
-// anthony makes the enemies maria makes move side to side and shoot at random intervals
+
+// anthony makes the enemies (maria makes move side to side) shoot at random intervals
+function createEnemyLaser($container, x, y) {
+    const $element = document.createElement("img");
+    $element.src = "img/enemy-fire.png";
+    $element.className = "enemy-laser";
+    $container.appendChild($element);
+    const laser = {x, y, $element};
+    GAME_STATE.enemeyLasers.push(laser);
+    setPosition($element, x,y);
+}
