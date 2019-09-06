4b6830c34af7d008e05fb3bc7d6fa7841ecd5ed1
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-05 17:00:02 -0700

    Housekeeping items
    Deleted duplicate/unneeded/useless code

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-// Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
-
 // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy)
 // document.getElementsByClassName("jumbotron-button").onclick = function(){
 //     if{};
 // }
 
-// // Maria make the enemies appear in 4 rows(1 row is the same kind of enemy) 
 // document.getElementsByClassName(enemy-area).style.transform = "translate(100px)";
 
 
 
 
 
-// anthony makes the enemies (maria makes move side to side) shoot at random intervals
+// anthony makes the enemies (that maria makes move side to side) shoot at random intervals
 function createEnemyLaser($container, x, y) {
     const $element = document.createElement("img");
     $element.src = "img/enemy-fire.png";
