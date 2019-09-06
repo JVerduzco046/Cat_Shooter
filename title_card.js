57407d786f5a76e6d8a0844fd7d674e5924928ce
Christian Gregorio <cgregorio026@west-mec.org>
2019-09-06 16:37:17 -0700

    File cleanup & function timer
    Created a timer which plays the entire js file after the screen fades out. Fixed a few spelling errors here and there, and got rid of a few unneeded files.

--- title_card.js
+++
@@ -1,6 +0,0 @@
-// Listens to see when the jumbotron button is pushed
-document.getElementById("start_button").addEventListener("mousedown", function(){
-    // Ensures that the Start Game button actually runs an event when clicked.
-    // this uses jQuery to select the intro card and make it fade out over 2000 milliseconds (2 seconds)
-    $("#introCard").fadeOut(2000);
-});
